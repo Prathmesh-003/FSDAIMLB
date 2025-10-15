7f95639a59e3ffbbe0ccdec2853df6ff34fa301c
Prathmesh <prathmeshs444@gmail.com>
2025-10-15 15:54:54 +0530

    Reactday1

--- CSS/javascript/Reactwork/App.jsx
+++ CSS/javascript/Reactwork/App.jsx
@@ -3,17 +3,14 @@ const root = ReactDOM.createRoot(parent);
 
 const h2 = React.createElement(
   "h2",
-  { style: { background: "brown", color: "white" } },
-  "welcome to react dom"
+  { style: { background: "brown", color: "white",textAlign:"center" } },
+  "  ABES ENGINEERING COLLEGE"
 );
 
 const l1 = React.createElement("li", {}, "java");
 const l2 = React.createElement("li", {}, "python");
 const l3 = React.createElement("li", {}, "React");
-
 const list = React.createElement("ul", { style: { background: "cyan" } }, l1, l2, l3);
-
-
 const nameAndPic = React.createElement(
   "div",
   {
@@ -24,12 +21,13 @@ const nameAndPic = React.createElement(
     },
   },
   React.createElement("img", {
-    src: "https://imgs.search.brave.com/a8s3pmo-DZYQF-1akKpxxwaOFK0QEtBkTUq5xu3B5e8/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9hcGku/bWVtZXMuY28uaW4v/bWVkaWEvbWVtZXN2/aWRlby90ZW1wbGF0/ZS9IYWtsYSUyMFNo/YWhydWtoJTIwS2hh/biUyME1lbWUlMjBU/ZW1wbGF0ZS5qcGc",
-    alt: "react",
-    style: { width: "100px", height: "100px", borderRadius: "50%" },
+    src: "https://imgs.search.brave.com/WG0qoXw5Z-34rcfdvahZGWR2f3RuX48RHv1GGs9o234/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzFjL2Y2/L2ZjLzFjZjZmYzk3/YzRmNzU0Yzg4MGYy/MTIzYmU2ZTI3Zjk3/LmpwZw",
+    alt: "react",style: { width: "100px", height: "100px", borderRadius: "70%" },
   }),
   React.createElement("h1", {}, "Prathmesh")
 );
 
 const wrapper = React.createElement("div", {}, h2, nameAndPic, list);
 root.render(wrapper);
+
+//jsx
