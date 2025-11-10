88fce8937508d67c99e9acc8f7b000bb27791819
Prathmesh <prathmeshs444@gmail.com>
2025-11-10 10:32:06 +0530

    Shopping Cart

---
+++ CSS/javascript/Reactwork/shoppingcart/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Login from './components/login'
+import Registration from './components/Registration'
+import Dashboarad from './components/Dashboard'
+import MainLayout from './components/MainLayout'
+
+function App() {
+  
+
+  return (
+    <>
+    <BrowserRouter >
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Registration/>}/>
+      <Route path='/dash' element={<Dashboarad/>}/>
+      <Route path='/mainlayout' element={<MainLayout/>}/>
+
+    </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
